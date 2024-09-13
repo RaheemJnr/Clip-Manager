1b1f17f7aed28f52833742e0f29c29458a145f9c
raheemjnr <mumedian6@gmail.com>
2024-09-13 18:37:43 +0100

    initial commit

---
+++ ClipManager/ClipManager.swift
@@ -0,0 +1,8 @@
+//
+//  ClipManager.swift
+//  ClipManager
+//
+//  Created by Raheem Jnr on 13/09/2024.
+//
+
+import Foundation
---
+++ ClipManager/Clipboarditem.swift
@@ -0,0 +1,8 @@
+//
+//  Clipboarditem.swift
+//  ClipManager
+//
+//  Created by Raheem Jnr on 13/09/2024.
+//
+
+import Foundation
