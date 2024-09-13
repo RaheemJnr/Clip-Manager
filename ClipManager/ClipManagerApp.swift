5b850fe28b711c5dfffe9a30e38fab5e08d3fa35
raheemjnr <mumedian6@gmail.com>
2024-09-13 11:28:33 +0100

    Initial Commit

---
+++ ClipManager/ClipManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipManagerApp.swift
+//  ClipManager
+//
+//  Created by Raheem Jnr on 13/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ClipManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
